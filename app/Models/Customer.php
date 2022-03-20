9dbdd3affd6065cc3c64c209356dcc8e7410fa23
Игорь Катин <igor.katin@soi-tech.com>
2022-03-20 17:16:47 +0200

    start locale

---
+++ app/Models/Customer.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Eloquent as Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+/**
+ * Class Customer
+ * @package App\Models
+ * @version March 20, 2022, 11:27 am UTC
+ *
+ */
+class Customer extends Model
+{
+    use SoftDeletes;
+
+    use HasFactory;
+
+    public $table = 'customers';
+    
+
+    protected $dates = ['deleted_at'];
+
+
+
+    public $fillable = [
+        
+    ];
+
+    /**
+     * The attributes that should be casted to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        
+    ];
+
+    /**
+     * Validation rules
+     *
+     * @var array
+     */
+    public static $rules = [
+        
+    ];
+
+    
+}
