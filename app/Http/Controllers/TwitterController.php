617d6dad0c6b3643195e86fb1a7fb3dc8ed82fe7
Игорь Катин <igor.katin@soi-tech.com>
2022-08-31 15:58:43 +0300

    socialupdate

--- app/Http/Controllers/TwitterController.php
+++ app/Http/Controllers/TwitterController.php
@@ -12,6 +12,7 @@ class TwitterController extends Controller
     /**
      * Login Using Twitter
      */
+    use \App\Http\Controllers\SocialNamesTrait;
     public function loginUsingTwitter()
     {
         return Socialite::driver('twitter')->redirect();
@@ -28,12 +29,19 @@ class TwitterController extends Controller
             // twitter provider doesn't use stateless.
             $user = Socialite::driver('twitter')->user();
 
+            $name = $this->getName($user->getName());
+            $email = $this->getEmail($user->getEmail());
+
             $saveUser = User::updateOrCreate([
                 'twitter_id' => $user->getId(),
             ], [
-                'name' => $user->getName(),
-                'email' => $user->getEmail(),
-                'password' => Hash::make($user->getName() . '@' . $user->getId()),
+//                'name' => $user->getName(),
+//                'email' => $user->getEmail(),
+//                'password' => Hash::make($user->getName() . '@' . $user->getId()),
+
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($name.'@'.$email)
             ]);
 
             Auth::loginUsingId($saveUser->id);
