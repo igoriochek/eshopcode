bb5a130c09600b86c7e89c64436b04e7174e65ac
Игорь Катин <igor.katin@soi-tech.com>
2022-09-29 13:50:39 +0300

    order log for new logs changed

--- app/Http/Controllers/PayController.php
+++ app/Http/Controllers/PayController.php
@@ -127,7 +127,8 @@ class PayController extends AppBaseController
                     $user = Auth::user();
 
                     if($user){
-                        $user->log("Created new Order ID:{$params['orderid']}");
+//                        $user->log("Created new Order ID:{$params['orderid']}");
+                        $user->log("Created new Order ID:{$newOrder->id}");
                     }
                     return 'OK';
                 }
