2b75f7e5afb1aa2652d774a92ef5d7e6c5edc179
MarSam <marius.sam98@gmail.com>
2022-10-25 14:04:41 +0300

    added background images

--- resources/views/user_views/section.blade.php
+++ resources/views/user_views/section.blade.php
@@ -1,4 +1,4 @@
-<section id="hero" class="background-image" data-background=url(../../img/header_bg.jpg) style="height: 470px">
+<section id="hero" class="background-image" data-background=url(../../img/dog.jpg) style="height: 470px">
     <div class="opacity-mask" data-opacity-mask="rgba(0, 0, 0, 0.6)">
         <div class="intro_title">
             <h3 class="animated fadeInDown">{{ $title ?? '' }}</h3>
