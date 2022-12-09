6b85669c2fd0dd82bb9455cf7e20903118ed5df7
Игорь Катин <igor.katin@soi-tech.com>
2022-12-09 12:12:02 +0200

    paysera

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="verify-paysera" content="4e65a95b838cbbe5d01d63bbd4df0860">
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
