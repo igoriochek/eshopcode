34f3801c47e1e557d0b4fb5e72ee13bbdec7baf4
Str3ltsov <andtaress@live.com>
2023-01-16 09:52:41 +0200

    Invoice fix

--- resources/views/orders/view_invoice.blade.php
+++ resources/views/orders/view_invoice.blade.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+<body class="d-block w-100 h-100">
 <div class="card">
     <div class="card-body">
         <div class="container mb-5 mt-3">
@@ -52,13 +60,13 @@
                         overflow-hidden
                         d-block
                         " data-ripple-color="light">
-{{--                                <img--}}
-{{--                                    class="img-fluid"--}}
-{{--                                    width="150"--}}
-{{--                                    height="150"--}}
-{{--                                    src="{{$orderItem->product->image ? $orderItem->product->image : 'https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg'}}"--}}
-{{--                                    alt="{{$orderItem->product->name}}--}}
-{{--                                    "/>--}}
+                                {{--                                <img--}}
+                                {{--                                    class="img-fluid"--}}
+                                {{--                                    width="150"--}}
+                                {{--                                    height="150"--}}
+                                {{--                                    src="{{$orderItem->product->image ? $orderItem->product->image : 'https://t4.ftcdn.net/jpg/04/00/24/31/360_F_400243185_BOxON3h9avMUX10RsDkt3pJ8iQx72kS3.jpg'}}"--}}
+                                {{--                                    alt="{{$orderItem->product->name}}--}}
+                                {{--                                    "/>--}}
 
                             </div>
                         </div>
@@ -79,18 +87,18 @@
                 @endforeach
                 <hr>
                 <div class="row">
-{{--                    <div class="col-xl-8">--}}
-{{--                        <p class="ms-3">PLACEHOLDER FOR ADDITIONAL NOTES AND PAYMENT INFO</p>--}}
-{{--                    </div>--}}
+                    {{--                    <div class="col-xl-8">--}}
+                    {{--                        <p class="ms-3">PLACEHOLDER FOR ADDITIONAL NOTES AND PAYMENT INFO</p>--}}
+                    {{--                    </div>--}}
                     <div class="col-xl-3">
-{{--                        <ul class="list-unstyled">--}}
-{{--                            <li class="text-muted ms-3"><span class="text-black me-4">{{__('table.subTotal')}}</span> BEFORE SHIPPING--}}
-{{--                            </li>--}}
-{{--                            <li class="text-muted ms-3 mt-2"><span class="text-black me-4">{{__('table.shipping')}}</span> INCLUDE PRICE--}}
-{{--                                HERE--}}
-{{--                            </li>--}}
-{{--                        </ul>--}}
-{{--                        {{ number_format((float)$order->sum, 2, '.', '') }}--}}
+                        {{--                        <ul class="list-unstyled">--}}
+                        {{--                            <li class="text-muted ms-3"><span class="text-black me-4">{{__('table.subTotal')}}</span> BEFORE SHIPPING--}}
+                        {{--                            </li>--}}
+                        {{--                            <li class="text-muted ms-3 mt-2"><span class="text-black me-4">{{__('table.shipping')}}</span> INCLUDE PRICE--}}
+                        {{--                                HERE--}}
+                        {{--                            </li>--}}
+                        {{--                        </ul>--}}
+                        {{--                        {{ number_format((float)$order->sum, 2, '.', '') }}--}}
                         <p class="text-black float-start"><span class="text-black me-3"> {{__('table.sum')}}</span><span
                                 style="font-size: 25px;"> {{ number_format($order->sum,2) }} €</span></p>
                     </div>
@@ -99,3 +107,5 @@
         </div>
     </div>
 </div>
+</body>
+</html>
