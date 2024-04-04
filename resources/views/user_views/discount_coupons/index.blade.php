2d84d01aa1e128cd4881577fd9297f779c30d15a
Str3ltsov <andtaress@live.com>
2024-04-04 16:27:13 +0300

    Discount coupons page design

--- resources/views/user_views/discount_coupons/index.blade.php
+++ resources/views/user_views/discount_coupons/index.blade.php
@@ -1,27 +1,34 @@
 @extends('layouts.app')
 
+@section('title', __('menu.discountCoupons'))
+
 @section('content')
-    <div class="container">
-        <div class="row">
-            <h5 class="text-uppercase">{{__('names.discountCoupons')}}</h5>
-            <div class="col-lg-12 mt-3">
-                <div class="row">
-                    @forelse( $discountCoupons as $prod )
-                        <div class="col-lg-6 col-md-12">
-                            <div class="my-3 mb-4 p-4" style="border-left: 7.5px solid #0e9f6e">
-                                <h5>{{__('names.discountCouponCode')}}: {{$prod->code}}</h5>
-                                <h6>{{__('names.discountCouponValue')}}: {{number_format($prod->value,2)}} EU</h6>
-                            </div>
-                        </div>
-                    @empty
-                        <span class="text-muted">{{ __('names.ndDiscountCoupons') }}</span>
-                    @endforelse
+    <div class="blog-area ptb-70">
+        <div class="container">
+            <div class="team-area pt-70 pb-45">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        @forelse($discountCoupons as $discountCoupon)
+                            @if (!$discountCoupon->used)
+                                <div class="col-lg-4 col-sm-6">
+                                    <div class="single-team-card">
+                                        <div class="content">
+                                            <h3>{{ __('names.discountCouponCode')}}: {{ $discountCoupon->code }}</h3>
+                                            <span>{{ __('names.discountCouponValue')}}: €{{ number_format($discountCoupon->value, 2)}}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endif
+                        @empty
+                            <span class="text-muted">{{ __('names.ndDiscountCoupons') }}</span>
+                        @endforelse
+                    </div>
                 </div>
-                <div class="d-flex justify-content-center mt-4">
-                    @if (!empty($discountCoupons->count()))
-                        {{ $discountCoupons->links() }}
-                    @endif
-                </div>
+            </div>
+            <div class="default-pagination mt-20">
+                @if (!empty($discountCoupons->count()))
+                    {{ $discountCoupons->onEachSide(1)->links() }}
+                @endif
             </div>
         </div>
     </div>
