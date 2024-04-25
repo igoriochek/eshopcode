98154e830fc6cbfe62b953c8b893435119acc411
Str3ltsov <andtaress@live.com>
2024-04-25 16:07:10 +0300

    Products page design

--- resources/views/user_views/product/filters.blade.php
+++ resources/views/user_views/product/filters.blade.php
@@ -1,63 +1,80 @@
 <form method="get" action="{{ route("userproducts") }}" id="mainForm">
-    <div class="shop-sidebar">
-        <div class="single-shop-sidebar-widget search-bar">
-            <div class="form-group">
-                <input type="text" name="filter[namelike]" class="form-control" id="filter[namelike]" 
-                    placeholder="{{ __('names.search') }}" value="{{ $filter["namelike"] ?? "" }}">
-                <button type="submit" class="search-btn pb-3">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </button>
+    <div class="tp-shop-sidebar tp-shop-sidebar-right mr-10">
+        <!-- search -->
+        <div class="tp-shop-widget mb-35">
+            <h3 class="tp-shop-widget-title no-border">{{ __('names.search') }}</h3>
+
+            <div class="tp-header-search-2 d-none d-sm-block">
+                <form action="#">
+                   <input type="text" name="filter[namelike]" id="filter[namelike]" placeholder="{{ __('names.product').'...' }}" value="{{ $filter["namelike"] ?? "" }}">
+                   <button type="submit">
+                      <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                         <path d="M9 17C13.4183 17 17 13.4183 17 9C17 4.58172 13.4183 1 9 1C4.58172 1 1 4.58172 1 9C1 13.4183 4.58172 17 9 17Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                         <path d="M18.9999 19L14.6499 14.65" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                      </svg>                                       
+                   </button>
+                </form>
+             </div>
+        </div>
+        <!-- filter -->
+        <div class="tp-shop-widget mb-35">
+            <h3 class="tp-shop-widget-title no-border">{{ __('names.filterByPrice') }}</h3>
+
+            <div class="tp-shop-widget-content">
+                <div class="tp-shop-widget-filter">
+                    <div class="range-slider">
+                        <div id="range-slider" class="slider mb-3 mt-1 mx-1" wire:ignore></div>
+                    </div>
+                    <div class="tp-shop-widget-filter-info d-flex align-items-center justify-content-between">
+                        <ul class="price-list d-flex justify-content-between list-unstyled">
+                            <li>
+                                <span>{{ __('names.from') }}:</span>
+                                <input type="text" id="filter[pricefrom]" name="filter[pricefrom]" 
+                                    readonly value="{{ $filter["pricefrom"] ?? '0' }}"
+                                    class="border-0 filter-by-price-number px-0" style="max-width: 40px; outline: none"/>
+                            </li>
+                            <li>
+                                <span>{{ __('names.to') }}:</span>
+                                <input type="text" id="filter[priceto]" name="filter[priceto]" 
+                                    readonly value="{{ $filter["priceto"] ?? '0' }}" 
+                                    class="border-0 filter-by-price-number px-0" style="max-width: 40px; outline: none"/>
+                            </li>
+                        </ul>
+                        <button class="tp-shop-widget-filter-btn" type="submit">
+                            {{ __('buttons.filter') }}
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="single-shop-sidebar-widget price-filter">
-            <h3>{{ __('names.filterByPrice') }}</h3>
-            <div class="range-slider">
-                <div id="range-slider" class="slider mb-3 mt-1 mx-1" wire:ignore></div>
-            </div>
-            <ul class="price-list d-flex justify-content-between">
-                <li>
-                    <span>{{ __('names.from') }} (€):</span>
-                    <input type="text" id="filter[pricefrom]" name="filter[pricefrom]" 
-                        readonly value="{{ $filter["pricefrom"] ?? '0' }}"
-                        class="border-0 text-start filter-by-price-number" style="max-width: 40px; outline: none"/>
-                </li>
-                <li>
-                    <span>{{ __('names.to') }} (€):</span>
-                    <input type="text" id="filter[priceto]" name="filter[priceto]" 
-                        readonly value="{{ $filter["priceto"] ?? '0' }}" 
-                        class="border-0 text-start filter-by-price-number" style="max-width: 40px; outline: none"/>
-                </li>
-            </ul>
-            <button class="default-btn style5" type="submit">
-                {{ __('buttons.filter') }}
-            </button>
-        </div>
-        <div class="single-shop-sidebar-widget color-and-item">
-            <h3>{{ __('names.categories') }}</h3>
-            <ul>
-                @forelse($categories as $category)
-                    <li>
-                        <div class="form-check">
-                            <input class="form-check-input" type="checkbox" id="category.{{ $category->id }}"
-                                    value="{{ $category->id }}" onclick="calc();"
-                                    @if ($filter && $filter["categories.id"])
+        <!-- status -->
+        <div class="tp-shop-widget mb-50">
+            <h3 class="tp-shop-widget-title">{{ __('names.categories') }}</h3>
+
+            <div class="tp-shop-widget-content">
+                <div class="tp-shop-widget-checkbox tp-shop-widget-categories">
+                    <ul class="filter-items filter-checkbox">
+                        @forelse ($categories as $category)
+                            <li class="filter-item checkbox">
+                                <input type="checkbox" id="category.{{ $category->id }}" value="{{ $category->id }}" onclick="calc();"
+                                    @if ($filter && isset($filter["categories.id"]))
                                         {{ in_array($category->id, $selCategories) ? "checked=\"checked\"" : ""}}
                                     @endif
-                            >
-                            <label class="form-check-label" for="category.{{ $category->id }}">
-                                {{ $category->name }}
-                            </label>
-                        </div>
-                    </li>
-                @empty
-                    <li>
-                        <span class="text-muted">{{ __('names.noCategories') }}</span>
-                    </li>
-                @endforelse
-            </ul>
+                                >
+                                <label for="category.{{ $category->id }}">
+                                    {{ $category->name }}
+                                </label>
+                            </li>
+                        @empty
+                            <li>
+                                <span class="text-muted">{{ __('names.noCategories') }}</span>
+                            </li>
+                        @endforelse
+                    </ul>
+                </div>
+            </div>
         </div>
+        <input type="hidden" id="filter[categories.id]" name="filter[categories.id]" value="{{ implode(",", $selCategories) }}">
+        <input type="hidden" id="order" name="order" value="{{ $selectedOrder }}">
     </div>
-    <input type="hidden" value="{{ implode(",", $selCategories) }}" 
-        name="filter[categories.id]" id="filter[categories.id]">
-    <input type="hidden" id="order" name="order" value="{{ $selectedOrder }}">
 </form>
