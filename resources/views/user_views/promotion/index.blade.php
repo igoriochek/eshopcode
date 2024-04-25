e077964882f994ec3f166d4d8a517ae2fc366f5a
Str3ltsov <andtaress@live.com>
2024-04-26 00:57:37 +0300

    Promotions and promotion page designs

--- resources/views/user_views/promotion/index.blade.php
+++ resources/views/user_views/promotion/index.blade.php
@@ -3,95 +3,79 @@
 @section('title', __('menu.promotions'))
 
 @section('content')
-    <div class="shop-area ptb-70">
+    <section class="tp-postbox-area pb-120">
         <div class="container">
-            <div class="row gap-5 gap-lg-0">
-                <div class="col-lg-9">
-                    <div class="row">
-                        @forelse ($promotions as $promotion)
-                            <div class="col-12 mb-4">
-                                <div class="shop-top-shorting-area">
-                                    <div class="row align-items-center">
-                                        <div class="col-lg-6 col-md-6">
-                                            <div class="shop-shorting-left-content">
-                                                <ul>
-                                                    <li>
-                                                        <h5 class="mb-1">{{ $promotion->name }}</h5>
-                                                        <p>
-                                                            {{ __('names.showing') }}
-                                                            @if ($promotions->currentPage() !== $promotions->lastPage())
-                                                                {{ ($promotions->count() * $promotions->currentPage() - $promotions->count() + 1).__('–').($promotions->count() * $promotions->currentPage()) }}
-                                                            @else
-                                                                @if ($promotions->total() - $promotions->count() === 0)
-                                                                    {{ $promotions->count() }}
-                                                                @else
-                                                                    {{ ($promotions->total() - $promotions->count()).__('–').$promotions->total() }}
-                                                                @endif
-                                                            @endif
-                                                            {{ __('names.of') }}
-                                                            {{ $promotions->total().' '.__('names.entries') }}
-                                                        </p>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                        <div class="col-lg-6 col-md-6">
-                                            <div class="shop-shorting-right-content">
-                                                <ul>
-                                                    <li>
-                                                        <a href="{{ route("promotion", ["id" => $promotion->id]) }}" class="default-btn style5">
-                                                            {{ __("names.more_for_promotions") }}
-                                                        </a>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div id="products-collections-filter" class="row justify-content-center">
+           <div class="row">
+              <div class="col-xl-9 col-lg-8">
+                 <div class="tp-postbox-wrapper pr-50">
+                    <p class="fs-6">
+                        {{ __('names.showing') }}
+                        @if ($promotions->currentPage() !== $promotions->lastPage())
+                            {{ ($promotions->count() * $promotions->currentPage() - $promotions->count() + 1).__('–').($promotions->count() * $promotions->currentPage()) }}
+                        @else
+                            @if ($promotions->total() - $promotions->count() === 0)
+                                {{ $promotions->count() }}
+                            @else
+                                {{ ($promotions->total() - $promotions->count()).__('–').$promotions->total() }}
+                            @endif
+                        @endif
+                        {{ __('names.of') }}
+                        {{ $promotions->total().' '.__('names.entries') }}
+                    </p>
+                    @forelse ($promotions as $promotion)
+                        <article class="tp-postbox-item format-image mb-50 transition-3">
+                            <div class="tp-postbox-content">
+                                <h5 class="tp-postbox-title pb-2">
+                                    {{ $promotion->name }}
+                                </h5>
+                                <div class="row">
                                     @forelse ($promotion->products as $product)
-                                        @include('user_views.product.product')
+                                        @include('user_views.product.grid_product')
                                         @if ($loop->iteration > 2)
+                                            <div class="tp-category-main-result text-center mb-35">
+                                                <p>{{ __('names.showing') }} {{ $loop->iteration }} {{ __('names.of') }} {{ count($promotion->products) }} {{ __('names.entries') }}</p>
+                                                <div class="tp-category-main-result-bar">
+                                                <span data-width="40%" style="width: 40%;"></span>
+                                                </div>
+                                            </div>
+                                            <div class="tp-category-main-more text-center mb-35">
+                                                <a href="{{ route("promotion", ["id" => $promotion->id]) }}" class="tp-load-more-btn">
+                                                    {{ __("names.more_for_promotions") }} 
+                                                </a>
+                                            </div>
                                             @break
                                         @endif
                                     @empty
                                         <span class="text-muted">{{ __('names.noProducts') }}</span>
                                     @endforelse
-                                </div>
+                                 </div>
                             </div>
-                        @empty
-                            <span class="text-muted">{{ __('names.noPromotions') }}</span>
-                        @endforelse
+                        </article>
+                    @empty
+                        <span class="text-muted">{{ __('names.noPromotions') }}</span>
+                    @endforelse
+                    <div class="tp-blog-pagination mt-50">
+                       <div class="tp-pagination">
+                            {{ $promotions->onEachSide(1)->links() }}
+                       </div>
                     </div>
-                    <div class="default-pagination mt-20">
-                        {{ $promotions->onEachSide(1)->links() }}
-                    </div>
-                </div>
-                <div class="col-lg-3">
-                    <div class="shop-sidebar">
-                        <div class="single-shop-sidebar-widget color-and-item">
-                            <h3>{{ __('names.promotions') }}</h3>
+                 </div>
+              </div>
+              <div class="col-xl-3 col-lg-4">
+                 <div class="tp-sidebar-wrapper tp-sidebar-ml--24">
+
+                    <!-- categories start -->
+                    <div class="tp-sidebar-widget widget_categories mb-35">
+                       <h3 class="tp-sidebar-widget-title">{{ __('names.promotions') }}</h3>
+                       <div class="tp-sidebar-widget-content">
                             @include('user_views.promotion.promotion_tree')
-                        </div>
+                       </div>
                     </div>
-                </div>
-            </div>
+                    <!-- categories end -->
+
+                 </div>
+              </div>
+           </div>
         </div>
-    </div>
+     </section>
 @endsection
-
-@push('css')
-    <style>
-        a {
-            color: #666666;
-
-            &:hover, &:focus {
-                color: #a10909;
-            }
-        }
-
-        .active {
-            color: #a10909;
-        }
-    </style>
-@endpush
--- resources/views/user_views/promotion/promotion_tree.blade.php
+++ resources/views/user_views/promotion/promotion_tree.blade.php
@@ -1,12 +1,10 @@
-<ul class="promotion-tree nav nav-list flex-column">
+<ul>
     @foreach($promotions as $promotion)
         <li>
-            <a href="{{ route("promotion", ["id" => $promotion->id] ) }}"
-               class="{{ substr(url()->current(), -1) == "$promotion->id" ? 'active' : '' }}">
-                <i class="fa-solid fa-angle-right pe-1"></i>
-                {{ $promotion->name }}
-                ({{ count($promotion->products) }})
+            <a href="{{ route("promotion", ["id" => $promotion->id] ) }}">
+                {{ $promotion->name }} 
+                <span>({{ count($promotion->products) }})</span>
             </a>
         </li>
     @endforeach
-</ul>
+ </ul>
