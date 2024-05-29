4236d3b17e5bcaa81fb0c05aaeacebc6aef263c3
Str3ltsov <andtaress@live.com>
2024-05-29 16:53:11 +0300

    Shopping cart page design

--- resources/views/user_views/cart/table.blade.php
+++ resources/views/user_views/cart/table.blade.php
@@ -1,63 +1,52 @@
 <table class="table">
     <thead>
-      <tr>
-        <th>{{ __('names.removeProduct') }}</th>
-        <th>{{ __('table.image') }}</th>
-        <th class="tp-cart-header-product">{{ __('names.product') }}</th>
-        <th class="tp-cart-header-quantity">{{ __('names.quantity') }}</th>
-        <th class="tp-cart-header-price">{{ __('names.price') }}</th>
-      </tr>
+        <tr>
+            <th class="product_remove"></th>
+            <th class="product-thumbnail"></th>
+            <th class="cart-product-name">{{ __('names.product') }}</th>
+            <th class="product-quantity">{{ __('names.quantity') }}</th>
+            <th class="product-subtotal">{{ __('names.price') }}</th>
+        </tr>
     </thead>
     <tbody>
-     @forelse($cartItems as $item)
-       <tr>
-        <!-- action -->
-        <td class="tp-cart-action">
-             {!! Form::open(['route' => ['userCartItemDestroy', $item->id], 'method' => 'delete']) !!}
-              <button type="submit" class="tp-cart-action-btn" onclick="return confirm('{{ __('messages.confirmDeleteProduct') }}')" style="background: transparent; border: none; outline: none; color: inherit; padding: 0; cursor: pointer;">
-                  <i class="pe-7s-trash" data-tippy="Remove" data-tippy-inertia="true" data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true" data-tippy-theme="sharpborder"></i>
-              </button>
-            {!! Form::close() !!}
-          </td>
-          <!-- img -->
-          <td class="tp-cart-img">
-             <a href="{{ route('viewproduct', $item['product']->id) }}"> 
-                 <img alt="{{ $item['product']->name }}" class="product-thumbnail-image" 
-                     src="@if ($item['product']->image) {{ $item['product']->image }} @else {{ asset('template/images/product/small-size/2-1-112x112.png') }} @endif">
-             </a>
-         </td>
-          <!-- title -->
-          <td class="product-name">
-             <a href="{{ route('viewproduct', $item['product']->id) }}">
-                 {{ $item['product']->name }}
-             </a>
-         </td>
-          <!-- quantity -->
-          <td class="product-name">
-             <span>{{ $item->count }}</span>
-          </td>
-          <!-- price -->
-          <td class="product-subtotal">
-             <span>€{{ number_format($item->price_current * $item->count, 2) }}</span>
-          </td>
-       </tr>
-     @empty
-       <tr>
-           <td colspan="5" class="text-center">{{ __('names.emptyCart') }}</td>
-       </tr>
-     @endforelse
+        @forelse($cartItems as $item)
+            <tr>
+                <td class="product_remove">
+                    {!! Form::open(['route' => ['userCartItemDestroy', $item->id], 'method' => 'delete']) !!}
+                    <button type="submit" class="btn fs-4" title="{{ __('names.removeProduct') }}"
+                        onclick="return confirm('{{ __('messages.confirmDeleteProduct') }}')">
+                        <i class="pe-7s-trash" data-tippy="{{ __('names.removeProduct') }}" data-tippy-inertia="true"
+                            data-tippy-animation="shift-away" data-tippy-delay="50" data-tippy-arrow="true"
+                            data-tippy-theme="sharpborder" tabindex="0"></i>
+                    </button>
+                    {!! Form::close() !!}
+                </td>
+                <td class="product-thumbnail">
+                    <a href="{{ route('viewproduct', $item['product']->id) }}">
+                        <img alt="{{ $item['product']->name }}" class="product-thumbnail-image"
+                            src="@if ($item['product']->image) {{ $item['product']->image }} @else {{ asset('template/images/product/small-size/2-1-112x112.png') }} @endif">
+                    </a>
+                </td>
+                <td class="product-name">
+                    <a href="{{ route('viewproduct', $item['product']->id) }}">
+                        {{ $item['product']->name }}
+                    </a>
+                </td>
+                <td class="quantity">
+                    <span class="amount fs-6">
+                        {{ $item->count }}
+                    </span>
+                </td>
+                <td class="product-subtotal">
+                    <span class="amount">
+                        €{{ number_format($item->price_current * $item->count, 2) }}
+                    </span>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td colspan="5" class="text-center">{{ __('names.emptyCart') }}</td>
+            </tr>
+        @endforelse
     </tbody>
 </table>
-
-@push('css')
-    <style>
-        .pe-7s-trash {
-            font-size: 20px;
-            color: #6c788c;
-        }
-
-        .pe-7s-trash:hover {
-          color: #ee3231;
-        }
-    </style>
-@endpush
--- resources/views/user_views/cart/view.blade.php
+++ resources/views/user_views/cart/view.blade.php
@@ -3,30 +3,46 @@
 @section('title', __('menu.cart'))
 
 @section('content')
-<section class="cart-area section-space-y-axis-100">
-   <div class="container">
-      <div class="row">
-         <div class="col-12">
-               <div class="table-content table-responsive">
-                  @include('user_views.cart.table')
-               </div>
-               <div class="row">
-                  <div class="col-md-5 ml-auto">
-                     <div class="cart-page-total">
-                        <h2>{{ __('names.overview') }}</h2>
-                        <ul>
-                           <li>{{ __('names.total') }}<span>€{{ $cart->sum ? number_format($cart->sum, 2) : '0'}}</span></li>
-                        </ul>
-                        @if (count($cartItems) > 0)
-                            <a href="{{ url('user/checkout') }}">
-                                {{ __('buttons.proceedToCheckout') }}
-                            </a>
-                        @endif
-                     </div>
-                  </div>
-               </div>
-         </div>
-      </div>
-   </div>
-</section>
+    <div class="cart-area section-space-y-axis-100">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div>
+                        <div class="table-content table-responsive">
+                            @include('user_views.cart.table')
+                        </div>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="coupon-all">
+                                    <div class="coupon1">
+                                        <a href="{{ route('userproducts') }}" class="btn btn-dark rounded-0 py-2">
+                                            {{ __('buttons.continueShopping') }}
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row mt-3">
+                            <div class="col-md-5 ml-auto">
+                                <div class="cart-page-total">
+                                    <h2>{{ __('names.overview') }}</h2>
+                                    <ul>
+                                        <li>
+                                            {{ __('names.total') }}
+                                            <span>€{{ $cart->sum ? number_format($cart->sum, 2) : '0' }}</span>
+                                        </li>
+                                    </ul>
+                                    @if (count($cartItems) > 0)
+                                        <a href="{{ url('user/checkout') }}">
+                                            {{ __('buttons.proceedToCheckout') }}
+                                        </a>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
