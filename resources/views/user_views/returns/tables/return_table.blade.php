161d4d2c6f6f024cc4b8a489b3fbb42c19674fd3
Str3ltsov <andtaress@live.com>
2024-05-31 16:57:00 +0300

    Returns page design

--- resources/views/user_views/returns/tables/return_table.blade.php
+++ resources/views/user_views/returns/tables/return_table.blade.php
@@ -1,32 +1,30 @@
-<table class="table">
-    <thead>
-       <tr>
-          <th scope="col">{{ __('names.return').' ID' }}</th>
-          <th scope="col">{{ __('table.status') }}</th>
-          <th scope="col">{{ __('table.sum') }}</th>
-          <th scope="col">{{ __('table.date') }}</th>
-          <th scope="col"></th>
-       </tr>
-    </thead>
+<table class="table table-bordered table-hover">
     <tbody>
-      @forelse($returns as $return)
-       <tr>
-          <th scope="row">{{ $return->id }}</th>
-          <td data-info="status">{{ __("status.".$return->status->name) }}</td>
-          <td data-info="sum">€{{ number_format($return->sum, 2) }}</td>
-          <td data-info="date">{{ $return->created_at->format('M d, Y') }}</td>
-          <td>
-              <a href="{{ route('viewreturn', [$return->id]) }}" class="tp-logout-btn">
-                  {{ __('buttons.view') }}
-              </a>
-          </td>
-       </tr>
-       @empty
-          <tr>
-              <td class="text-muted" colspan="5">
-                  {{ __('names.noReturns') }}
-              </td>
-          </tr>
-      @endforelse
+        <tr>
+            <th>{{ __('names.return') . ' ID' }}</th>
+            <th>{{ __('table.status') }}</th>
+            <th>{{ __('table.sum') }}</th>
+            <th>{{ __('table.date') }}</th>
+            <th></th>
+        </tr>
+        @forelse($returns as $return)
+            <tr>
+                <td>{{ $return->id }}</td>
+                <td>{{ __('status.' . $return->status->name) }}</td>
+                <td>€{{ number_format($return->sum, 2) }}</td>
+                <td>{{ $return->created_at->format('M d, Y') }}</td>
+                <td>
+                    <a href="{{ route('viewreturn', [$return->id]) }}" class="btn btn-dark btn-primary-hover">
+                        {{ __('buttons.view') }}
+                    </a>
+                </td>
+            </tr>
+        @empty
+            <tr>
+                <td class="text-muted" colspan="5">
+                    {{ __('names.noReturns') }}
+                </td>
+            </tr>
+        @endforelse
     </tbody>
 </table>
