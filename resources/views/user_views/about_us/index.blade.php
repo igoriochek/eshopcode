c96aa3cc86200c6eb6885ce97ce120aee14b33de
edgarsem <90824295+edgarsem@users.noreply.github.com>
2024-06-05 19:52:16 +0300

    About us, eu projects, fb data deletion, terms of service, privacy policy pages designs

--- resources/views/user_views/about_us/index.blade.php
+++ resources/views/user_views/about_us/index.blade.php
@@ -3,20 +3,17 @@
 @section('title', __('menu.aboutUs'))
 
 @section('content')
-    <section class="tp-shop-area pb-120">
-        <div class="container">
+<div class="about-banner different-bg-position section-space-y-axis-100">
+    <div class="container">
         <div class="row">
-                <div class="col-12 mb-4">
+        <div class="col-12 mb-4">
                     @include('flash_messages')
                 </div>
-            <div class="col-12">
-                <div class="tp-shop-main-wrapper">
-                    <div class="tp-shop-items-wrapper tp-shop-item-primary">
-                    <div class="tab-content" id="productTabContent">
-                        <div class="tab-pane fade show active" id="grid-tab-pane" role="tabpanel" aria-labelledby="grid-tab" tabindex="0">
-                            <div class="row">
-                                @if (app()->getLocale() == 'lt')
-                                    <p>
+            <div class="col-lg-12">
+                <div class="about-banner-content text-center section-space-bottom-95">
+                    <div class="section-title">
+                        @if (app()->getLocale() == 'lt')
+                                    <p class="short-desc mb-0">
                                         UAB "Dts Solutions" – kokybiškas buhalterines ir teisines paslaugas teikianti įmonė.<br><br>
                                         Nėra tokios organizacijos, kur buhalterija ir vidaus auditas nebūtų svarbūs. Šitas pagrindinės vidinės veiklos, padedančios įmonėms ir organizacijoms veikti teisėtas ir veiksmingas.<br><br>
                                         Mūsų patyrę partneriai įvairiose šalyse padarys viską, kad žinotų ir suprastų jūsų verslą ir yra tam pasiruošę teikti platų kokybiškų paslaugų spektrą.<br><br>
@@ -26,7 +23,7 @@
                                         Mūsų projektas leis įmonei efektyviau parduoti savo paslaugas, teikti greičiau ir geriau klientų aptarnavimas, sutrumpės paslaugų įgyvendinimo laikas, leis aptarnauti daugiau klientų, o tai užtikrins įmonės pajamų augimas.<br><br>
                                     </p>
                                 @elseif (app()->getLocale() == "ru")
-                                    <p>
+                                    <p class="short-desc mb-0">
                                         ЗАО "Dts Solutions" - это компания, предоставляющая высококачественные бухгалтерские и юридические услуги.<br><br>
                                         Нет такой организации, где бухгалтерия и внутренний аудит не имели бы значения. Это основная внутренняя деятельность, которая помогает предприятиям и организациям работать законно и эффективно.<br><br>
                                         Наши опытные партнеры в разных странах сделают все, чтобы узнать и понять ваш бизнес и готовы предоставить для этого широкий спектр качественных услуг.<br><br>
@@ -36,7 +33,7 @@
                                         Наш проект позволит компании более эффективно продавать свои услуги, обеспечить более быстрое и качественное обслуживание клиентов, сократить время на реализацию услуг, позволит обслуживать больше клиентов, что обеспечит рост выручки компании.<br><br>
                                     </p>
                                 @else
-                                    <p>
+                                    <p class="short-desc mb-0">
                                         UAB "Dts Solutions" is a company providing high-quality accounting and legal services.<br><br>
                                         There is no such organization where bookkeeping and internal audit would not matter. This is the core internal activity that helps businesses and organizations operate legally and effectively.<br><br>
                                         Our experienced partners in different countries will do everything to know and understand your business and are ready to provide a wide range of quality services for this.<br><br>
@@ -46,13 +43,12 @@
                                         Our project will allow the company to sell its services more efficiently, provide faster and better customer service, reduce the time for the implementation of services, allow to serve more customers, which will ensure the growth of the company's revenue.<br><br>
                                     </p>
                                 @endif
-                            </div>
-                        </div>
-                        </div>                            
+                    
+                    
                     </div>
                 </div>
-            </div>          
+            </div>
         </div>
-        </div>
-    </section>
+    </div>
+</div>
 @endsection
--- resources/views/user_views/fb_data_deletion/index.blade.php
+++ resources/views/user_views/fb_data_deletion/index.blade.php
@@ -3,7 +3,52 @@
 @section('title', __('menu.fbDataDeletion'))
 
 @section('content')
-    <section class="tp-shop-area pb-120">
+
+<div class="about-banner different-bg-position section-space-y-axis-100">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 mb-4">
+                @include('flash_messages')
+            </div>
+            <div class="col-lg-12">
+                <div class="about-banner-content text-start section-space-bottom-95">
+                    <div class="section-title">
+                        @if (config('app.locale') == 'lt')
+                        <p class="short-desc mb-0">Dts Solutions svetainėje naudojamas Facebook prisijungimas. Pagal Facebook politiką turime pateikti Duomenų ištrynimo instrukcijos URL adresą.</p>
+                        <p class="short-desc mb-0">Jei norite ištrinti savo veiklą Dts Solutions svetainėje, galite pašalinti savo informaciją atlikdami šiuos veiksmus:</p>
+                        <div class="ps-4 pb-1">1. Eikite į Facebook Account's Setting & Privacy (Facebook paskyros nustatymai ir privatumas). Spustelėkite Settings (Nustatymai).</div>
+                        <div class="ps-4 pb-1">2. Ieškokite Apps and Websites (Programėlės ir svetainės) ir pamatysite visas programėles ir svetaines, kurias susiejote su savo Facebook paskyra.</div>
+                        <div class="ps-4 pb-1">3. Ieškokite ir paieškos juostoje spustelėkite Dts Solutions.</div>
+                        <div class="ps-4 pb-1">4. Slinkite ir spustelėkite Remove (Pašalinti).</div>
+                        <div class="ps-4 pb-1">5. Sveikiname, jūs sėkmingai pašalinote savo veiklą mūsų svetainėje.</div>
+                        @elseif (config('app.locale') == 'ru')
+                        <p class="short-desc mb-0">На сайте Dts Solutions используется логин Facebook. Согласно политике Facebook, мы должны предоставить URL-адрес инструкции по удалению данных.</p>
+                        <p class="short-desc mb-0">Если вы хотите удалить свою активность на сайте Dts Solutions, вы можете удалить свою информацию, выполнив следующие действия:</p>
+                        <div class="ps-4 pb-1">1. Перейдите в раздел Facebook Account's Setting & Privacy (Настройки и конфиденциальность учетной записи Facebook). Нажмите Settings (Настройки).</div>
+                        <div class="ps-4 pb-1">2. Найдите Apps and Websites (Приложения и веб-сайты), и вы увидите все приложения и веб-сайты, которые вы связали со своей учетной записью Facebook.</div>
+                        <div class="ps-4 pb-1">3. В строке поиска выберите Dts Solutions.</div>
+                        <div class="ps-4 pb-1">4. Прокрутите страницу и нажмите Remove (Удалить).</div>
+                        <div class="ps-4 pb-1">5. Поздравляем, вы успешно удалили свою деятельность с нашего сайта.</div>
+                        @else
+                        <p class="short-desc mb-0">Dts Solutions website uses the Facebook login. As per Facebook policy, we must provide Data Deletion Instructions URL.</p>
+                        <p class="short-desc mb-0">If you want to delete your activities for Dts Solutions website, you can remove your information by following these steps:</p>
+                        <div class="ps-4 pb-1">1. Go to your Facebook Account's Setting & Privacy. Click Settings.</div>
+                        <div class="ps-4 pb-1">2. Look for Apps and Websites and you will see all apps and websites you linked with your Facebook.</div>
+                        <div class="ps-4 pb-1">3. Search and click Dts Solutions in the search bar.</div>
+                        <div class="ps-4 pb-1">4. Scroll and click Remove.</div>
+                        <div class="ps-4 pb-1">5. Congratulations, you have successfully removed your activity from our website.</div>
+                        @endif
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<!-- <section class="tp-shop-area pb-120">
         <div class="container">
             <div class="row">
                     <div class="col-12 mb-4">
@@ -48,5 +93,5 @@
                 </div>          
             </div>
         </div>
-    </section>
+    </section> -->
 @endsection
