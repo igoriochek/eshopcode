d9f4fb1cb16bb73ab634ad8b0261d35b544928c3
edgarsem <90824295+edgarsem@users.noreply.github.com>
2024-10-21 10:38:49 +0300

    Review button fix

--- resources/views/user_views/product/product_tabs.blade.php
+++ resources/views/user_views/product/product_tabs.blade.php
@@ -13,74 +13,74 @@
 </li>
 </ul>
 <div class="tab-content">
-        <div class="tab-pane fade active show" id="description" role="tabpanel" aria-labelledby="description-tab" tabindex="0">
-            <div class="description-content">
-                {!! $product->description !!}
-            </div>
+    <div class="tab-pane fade active show" id="description" role="tabpanel" aria-labelledby="description-tab" tabindex="0">
+        <div class="description-content">
+            {!! $product->description !!}
         </div>
-        <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab" tabindex="0">
-            <div class="review-content">
-                <div class="all-review-content">
-                    <h3>{{ __('names.reviews') }}</h3>
-                    @forelse ($product->ratings as $rating)
-                        <div class="single-review ps-1">
-                            <div class="content">
-                                <div class="rating flex-row justify-content-start">
-                                    @for($i = 1; $i <= 5; $i++)
-                                        <i class="product-rating-star @if ($rating->value >= $i) fa-solid fa-star
+    </div>
+    <div class="tab-pane fade" id="reviews" role="tabpanel" aria-labelledby="reviews-tab" tabindex="0">
+        <div class="review-content">
+            <div class="all-review-content">
+                <h3>{{ __('names.reviews') }}</h3>
+                @forelse ($product->ratings as $rating)
+                <div class="single-review ps-1">
+                    <div class="content">
+                        <div class="rating flex-row justify-content-start">
+                            @for($i = 1; $i <= 5; $i++)
+                                <i class="product-rating-star @if ($rating->value >= $i) fa-solid fa-star
                                             @elseif ($rating->value >= $i - .5) fa-solid fa-star-half-stroke
                                             @else fa-regular fa-star 
                                             @endif"></i>
-                                    @endfor
-                                </div>
-                                <h5>{{ $rating->user->name }}</h5>
-                                <span class="date">{{ $rating->created_at->format('F j, Y') }}</span>
-                                <p>{{ $rating->description }}</p>
-                            </div>
+                                @endfor
                         </div>
-                    @empty
-                        <span class="text-muted">{{ __('names.noReviews') }}</span>
-                    @endforelse
+                        <h5>{{ $rating->user->name }}</h5>
+                        <span class="date">{{ $rating->created_at->format('F j, Y') }}</span>
+                        <p>{{ $rating->description }}</p>
+                    </div>
                 </div>
-                <div id="write" class="review-form">
-                    <h3>{{ __('names.addReview') }}</h3>
-                    @auth
-                        <form class="review-product">
-                            <div class="form-group">
-                                <label>{{ __('names.rating').'*' }}</label>
-                                <div class="rating" style="gap: 5px">
-                                    <input type="radio" name="rating" value="5" id="5"><label for="5">
-                                        <i class="fa-regular fa-star text-warning"></i>
-                                    </label>
-                                    <input type="radio" name="rating" value="4" id="4"><label for="4">
-                                        <i class="fa-regular fa-star text-warning"></i>
-                                    </label>
-                                    <input type="radio" name="rating" value="3" id="3"><label for="3">
-                                        <i class="fa-regular fa-star text-warning"></i>
-                                    </label>
-                                    <input type="radio" name="rating" value="2" id="2"><label for="2">
-                                        <i class="fa-regular fa-star text-warning"></i>
-                                    </label>
-                                    <input type="radio" name="rating" value="1" id="1"><label for="1">
-                                        <i class="fa-regular fa-star text-warning"></i>
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label>{{ __('names.review').'*' }}</label>
-                                <textarea class="form-control" rows="8" id="comment" name="comment"></textarea>
-                            </div>
-                            <button class="default-btn style5 product-reviews-add-review-submit" type="submit">
-                                {{ __('buttons.submit') }}
-                            </button>
-                        </form>
-                    @else
-                        <span class="text-muted">{{ __('names.loginToReview') }}</p>
+                @empty
+                <span class="text-muted">{{ __('names.noReviews') }}</span>
+                @endforelse
+            </div>
+            <div id="write" class="review-form">
+                <h3>{{ __('names.addReview') }}</h3>
+                @auth
+                <form class="review-product">
+                    <div class="form-group">
+                        <label>{{ __('names.rating').'*' }}</label>
+                        <div class="rating" style="gap: 5px">
+                            <input type="radio" name="rating" value="5" id="5"><label for="5">
+                                <i class="fa-regular fa-star text-warning"></i>
+                            </label>
+                            <input type="radio" name="rating" value="4" id="4"><label for="4">
+                                <i class="fa-regular fa-star text-warning"></i>
+                            </label>
+                            <input type="radio" name="rating" value="3" id="3"><label for="3">
+                                <i class="fa-regular fa-star text-warning"></i>
+                            </label>
+                            <input type="radio" name="rating" value="2" id="2"><label for="2">
+                                <i class="fa-regular fa-star text-warning"></i>
+                            </label>
+                            <input type="radio" name="rating" value="1" id="1"><label for="1">
+                                <i class="fa-regular fa-star text-warning"></i>
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label>{{ __('names.review').'*' }}</label>
+                        <textarea class="form-control" rows="8" id="comment" name="comment"></textarea>
+                    </div>
+                    <button class="default-btn style5 product-reviews-add-review-submit" type="submit">
+                        {{ __('buttons.submit') }}
+                    </button>
+                </form>
+                @else
+                <span class="text-muted">{{ __('names.loginToReview') }}</p>
                     @endauth
-                </div>
             </div>
         </div>
     </div>
+</div>
 </div> --}}
 
 <ul class="nav justify-content-center feature-tab-list-2 mt-4" id="nav-tab-2" role="tablist">
@@ -256,7 +256,7 @@
                         </div>
                         <div class="col-12">
                             <button type="button" type="submit" class="btn btn-primary mt-4 d-block w-100" id="product-reviews-add-review-submit">
-                                {{ __('buttons.submit') }}
+                                {{ __('buttons.postReview') }}
                             </button>
                         </div>
                     </div>
