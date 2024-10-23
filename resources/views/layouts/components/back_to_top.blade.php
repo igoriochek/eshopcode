42957259c810fb3ed81850de651307aa5e9a61af
edgarsem <90824295+edgarsem@users.noreply.github.com>
2024-10-23 12:37:43 +0300

    Back to top element design

--- resources/views/layouts/components/back_to_top.blade.php
+++ resources/views/layouts/components/back_to_top.blade.php
@@ -1,1 +1,44 @@
-<a href="#top" class="back-to-top" id="backto-top"><i class="fal fa-arrow-up"></i></a>
+<a id="scrollUp" href="#top" style="position: fixed; z-index: 214; display: none;">
+    <i class="fas fa-arrow-up"></i>
+</a>
+
+@push('css')
+<style>
+    #scrollUp {
+        bottom: 40px;
+        right: 40px;
+        background-color: #007bff;
+        color: white;
+        padding: 10px;
+        border-radius: 50%;
+        font-size: 20px;
+        display: none;
+        opacity: 0;
+        transition: opacity 0.3s ease-in-out;
+    }
+
+    #scrollUp.show {
+        display: block;
+        opacity: 1;
+    }
+</style>
+@endpush
+
+@push('scripts')
+<script>
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+            $('#scrollUp').addClass('show').fadeIn();
+        } else {
+            $('#scrollUp').removeClass('show').fadeOut();
+        }
+    });
+
+    $('#scrollUp').click(function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: 0
+        }, '300');
+    });
+</script>
+@endpush
