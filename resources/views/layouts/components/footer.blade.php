4e4a1488070483d1dabbff8756f31271a2ede546
edgarsem <90824295+edgarsem@users.noreply.github.com>
2024-10-23 12:21:08 +0300

    Footer design

--- resources/views/layouts/components/footer.blade.php
+++ resources/views/layouts/components/footer.blade.php
@@ -1,186 +1,197 @@
-<footer class="axil-footer-area footer-style-2">
-    <!-- Start Footer Top Area  -->
-    <div class="footer-top separator-top">
+<footer>
+    <div class="address py-6rem bg-white">
         <div class="container">
             <div class="row">
-                <!-- Start Single Widget  -->
-                <div class="col-lg-3 col-sm-6">
-                    <div class="axil-footer-widget">
-                        <h5 class="widget-title">Support</h5>
-                        <div class="inner">
-                            <p>	Veisiejų g. 12-40, <br>
-                            LT-66241 Druskininkai
-                            </p>
-                            <ul class="support-list-item">
-                                <li><a href="mailto:example@domain.com"><i class="fal fa-envelope-open"></i> example@domain.com</a></li>
-                                <li><a href="tel:+37068629686"><i class="fal fa-phone-alt"></i> +370 686 29686</a></li>
-                            </ul>
+                <div class="col-12 col-sm-7 col-md-4 my-3">
+                    <div class="address-widget">
+                        <div class="media">
+                            <span class="address-icon">
+                                <i class="ion-ios-location-outline"></i>
+                            </span>
+                            <div class="media-body">
+                                <h4 class="title">Veisiejų g. 12-40, LT-66241 Druskininkai</h4>
+                                <p class="text">{{ __('footer.address') }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- End Single Widget  -->
-                <!-- Start Single Widget  -->
-                <div class="col-lg-3 col-sm-6">
-                    <div class="axil-footer-widget">
-                        <h5 class="widget-title">{{ __('footer.information') }}</h5>
-                        <div class="inner">
-                            <ul>
-                                <li>
-                                    <a href="{{ url('/about_us') }}" class="{{ request()->is('about_us') ? 'active' : '' }}">
-                                        {{ __('menu.aboutUs') }}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('/termsofservice') }}" class="{{ request()->is('termsofservice') ? 'active' : '' }}">
-                                        {{ __('menu.termsofservice') }}
-                                    </a>
-                                </li>   
-                                <li>
-                                    <a href="{{ url('/policy') }}" class="{{ request()->is('termsofservice') ? 'active' : '' }}">
-                                        {{ __('menu.policy') }}
-                                    </a>
-                                </li>   
-                                <li>
-                                    <a href="{{ url('/eu_projects') }}" class="{{ request()->is('eu_projects') ? 'active' : '' }}">
-                                        {{ __('menu.euProjects') }}
-                                    </a>
-                                </li>   
-                                <li>
-                                    <a href="{{ url('/fbdatadeletion') }}" class="{{ request()->is('fbdatadeletion') ? 'active' : '' }}">
-                                        {{ __('menu.fbDataDeletion') }}
-                                    </a>
-                                </li>   
-                            </ul>
+                <div class="col-12 col-sm-5 col-md-4 my-3">
+                    <div class="address-widget">
+                        <div class="media">
+                            <span class="address-icon">
+                                <i class="ion-ios-email-outline"></i>
+                            </span>
+                            <div class="media-body">
+                                <h4 class="title"><a href="mailto:example@domain.com">example@domain.com</a></h4>
+                                <p class="text">{{ __('footer.email') }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- End Single Widget  -->
-                <!-- Start Single Widget  -->
-                <div class="col-lg-3 col-sm-6">
-                    <div class="axil-footer-widget">
-                        <h5 class="widget-title">{{ __('footer.menu') }}</h5>
-                        <div class="inner">
-                            <ul>
-                                <li>
-                                    <a href="{{ url('/products') }}">
-                                        {{ __('menu.products') }}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('/rootcategories') }}">
-                                        {{ __('menu.categories') }}
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="{{ url('/promotions') }}">
-                                        {{ __('menu.promotions') }}
-                                    </a>
-                                </li>
-                                @auth
-                                    <li>
-                                        <a href="{{ url('/user/discountCoupons') }}">
-                                            {{ __('menu.discountCoupons') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/user/messenger') }}">
-                                            {{ __('menu.messenger') }}
-                                        </a>
-                                    </li>
-                                @endauth
-                            </ul>
+                <div class="col-12 col-sm-6 col-md-4 my-3">
+                    <div class="address-widget">
+                        <div class="media">
+                            <span class="address-icon">
+                                <i class="ion-ios-telephone-outline"></i>
+                            </span>
+                            <div class="media-body">
+                                <h4 class="title"><a href="tel:+37068629686"> +370 686 29686</a></h4>
+                                <p class="text">{{ __('footer.phone') }}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <!-- End Single Widget  -->
-                <!-- Start Single Widget  -->
-                <div class="col-lg-3 col-sm-6">
-                    <div class="axil-footer-widget">
-                        <h5 class="widget-title">{{ __('footer.account') }}</h5>
-                        <div class="inner">
-                            <ul>
-                                @auth
-                                    <li>
-                                        <a href="{{ url('/user/viewcart') }}">
-                                            {{ __('menu.cart') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/user/rootorders') }}">
-                                            {{ __('menu.orders') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/user/rootoreturns') }}">
-                                            {{ __('menu.returns') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ url('/user/userprofile') }}">
-                                            {{ __('menu.profile') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                            @csrf
-                                            <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                                {{ __('menu.logout') }}
-                                            </a>
-                                        </form>
-                                    </li>
-                                @else
-                                    <li>
-                                        <a href="{{ route('login') }}">
-                                            {{ __('buttons.login') }}
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="{{ route('register') }}">
-                                            {{ __('buttons.register') }}
-                                        </a>
-                                    </li>
-                                @endauth
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!-- End Single Widget  -->
             </div>
         </div>
     </div>
-    <!-- End Footer Top Area  -->
-    <!-- Start Copyright Area  -->
-    <div class="copyright-area copyright-default separator-top">
+    <div class="footer-bottom pb-5 bg-white">
         <div class="container">
-            <div class="row align-items-center">
-                {{-- <div class="col-xl-4">
-                    <div class="social-share">
-                        <a href="#"><i class="fab fa-facebook-f"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fa-brands fa-x-twitter"></i></a>
-                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
-                        <a href="#"><i class="fab fa-discord"></i></a>
-                    </div>
-                </div> --}}
-                <div class="col-xl-6 col-lg-12">
-                    <div class="copyright-left d-flex flex-wrap justify-content-center justify-content-lg-start">
-                        <ul class="quick-link">
-                            <li>{{ __('footer.copyright') }}</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-12">
-                    <div class="copyright-right d-flex flex-wrap justify-content-xl-end justify-content-center align-items-center">
-                        <span class="card-text">{{ __('footer.acceptPayments') }}</span>
-                        <ul class="payment-icons-bottom quick-link">
+            <div class="row">
+                <div class="col-12 col-sm-6 col-lg-4 mb-5">
+                    <div class="footer-widget">
+                        <div class="border-bottom cbb1 mb-3rem">
+                            <div class="section-title pb-4 pb-md-4 position-relative">
+                                <h2 class="title">{{ __('footer.information') }}</h2>
+                            </div>
+                        </div>
+                        <ul class="footer-menu">
                             <li>
-                                <img src="{{ asset('images/1_Paysera logo for light background.svg') }}" alt="image" width="80px">
+                                <a href="{{ url('/about_us') }}">
+                                    {{ __('menu.aboutUs') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/termsofservice') }}">
+                                    {{ __('menu.termsofservice') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/policy') }}">
+                                    {{ __('menu.policy') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/eu_projects') }}">
+                                    {{ __('menu.euProjects') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/fbdatadeletion') }}">
+                                    {{ __('menu.fbDataDeletion') }}
+                                </a>
                             </li>
                         </ul>
                     </div>
                 </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-5">
+                    <div class="footer-widget">
+                        <div class="border-bottom cbb1 mb-3rem">
+                            <div class="section-title pb-4 pb-md-4 position-relative">
+                                <h2 class="title">{{ __('footer.menu') }}</h2>
+                            </div>
+                        </div>
+                        <ul class="footer-menu">
+                            <li>
+                                <a href="{{ url('/products') }}">
+                                    {{ __('menu.products') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/rootcategories') }}">
+                                    {{ __('menu.categories') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/promotions') }}">
+                                    {{ __('menu.promotions') }}
+                                </a>
+                            </li>
+                            @auth
+                            <li>
+                                <a href="{{ url('/user/discountCoupons') }}">
+                                    {{ __('menu.discountCoupons') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/user/messenger') }}">
+                                    {{ __('menu.messenger') }}
+                                </a>
+                            </li>
+                            @endauth
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-6 col-lg-4 mb-5">
+                    <div class="footer-widget">
+                        <div class="border-bottom cbb1 mb-3rem">
+                            <div class="section-title pb-4 pb-md-4 position-relative">
+                                <h2 class="title">{{ __('footer.account') }}</h2>
+                            </div>
+                        </div>
+                        <ul class="footer-menu">
+                            @auth
+                            <li>
+                                <a href="{{ url('/user/viewcart') }}">
+                                    {{ __('menu.cart') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/user/rootorders') }}">
+                                    {{ __('menu.orders') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/user/rootoreturns') }}">
+                                    {{ __('menu.returns') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ url('/user/userprofile') }}">
+                                    {{ __('menu.profile') }}
+                                </a>
+                            </li>
+                            <li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
+                                    @csrf
+                                    <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('menu.logout') }}
+                                    </a>
+                                </form>
+                            </li>
+                            @else
+                            <li>
+                                <a href="{{ route('login') }}">
+                                    {{ __('buttons.login') }}
+                                </a>
+                            </li>
+                            <li>
+                                <a href="{{ route('register') }}">
+                                    {{ __('buttons.register') }}
+                                </a>
+                            </li>
+                            @endauth
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-    <!-- End Copyright Area  -->
+    <div class="coppy-right pt-2rem pb-2rem bg-light">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <div class="text-center text-md-start">
+                        <p class="copyright-text mb-3 mb-md-0">
+                            {{ __('footer.copyright') }}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-12 col-md-6">
+                    <div class="text-center text-md-end">
+                        <img src="{{ asset('images/1_Paysera logo for light background.svg') }}" alt="image" width="80px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
