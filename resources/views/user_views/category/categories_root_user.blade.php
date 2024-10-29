7bc8eb6eca11d716fefcdccd8ae674380098beb6
edgarsem <90824295+edgarsem@users.noreply.github.com>
2024-10-29 15:26:57 +0200

    Discount Coupons page design

--- resources/views/user_views/category/categories_root_user.blade.php
+++ resources/views/user_views/category/categories_root_user.blade.php
@@ -3,38 +3,41 @@
 @section('title', __('menu.categories'))
 
 @section('content')
-    <div class="whish-list-section pb-6rem">
-        <div class="container">
-            <div class="row gap-5 gap-lg-0">
-                <div class="col-12">
-                    <div class="shop-sidebar">
-                        <div class="single-shop-sidebar-widget color-and-item pb-1">
-                            <h3 class="title text-capitalize">{{ __('names.categories') }}</h3>
-                            @include('user_views.category.category_tree')
+<div class="whish-list-section pb-6rem">
+    <div class="container">
+        <div class="row gap-5 gap-lg-0">
+            <div class="col-12">
+                <div class="shop-sidebar">
+                    <div class="border-bottom cbb1 mb-3rem">
+                        <div class="section-title pb-4 pb-md-4 position-relative">
+                            <h2 class="title" style="border-bottom: 0px solid #e5e5e5; margin-bottom: 0px;">{{ __('names.categories') }}</h2>
                         </div>
                     </div>
+                    @include('user_views.category.category_tree')
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('css')
-    <style>
-        .title {
-            margin-bottom: 16px;
-        }
+<style>
+    .title {
+        margin-bottom: 16px;
+    }
 
-        a {
-            color: #666666;
+    a {
+        color: #666666;
 
-            &:hover, &:focus {
-                color: #a10909;
-            }
-        }
-
-        .active {
+        &:hover,
+        &:focus {
             color: #a10909;
         }
-    </style>
+    }
+
+    .active {
+        color: #a10909;
+    }
+</style>
 @endpush
--- resources/views/user_views/discount_coupons/index.blade.php
+++ resources/views/user_views/discount_coupons/index.blade.php
@@ -3,85 +3,83 @@
 @section('title', __('menu.discountCoupons'))
 
 @section('content')
-    <div class="axil-single-product-area axil-section-gap bg-color-white pb--120">
-        <div class="container">
-            <div class="row gap-5 gap-lg-0">
-                <div class="col-12">
-                    <div class="axil-shop-top mb--40">
-                        <div class="row align-items-center">
-                            <div class="col-lg-6 col-md-6">
-                                <div class="shop-shorting-left-content">
-                                    <h4 class="mb-1">{{ __('menu.discountCoupons') }}</h4>
-                                    <p class="filter-results">
-                                        @if (count($discountCoupons) > 0)
+<div class="axil-single-product-area axil-section-gap bg-color-white pb--120">
+    <div class="container">
+        <div class="row gap-5 gap-lg-0">
+            <div class="col-12">
+                <h1 class="mb-3">{{ __('menu.discountCoupons') }}</h1>
+                <div class="grid-nav-wraper bg-light mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-12 col-md-6 mb-3 mb-md-0">
+                            <nav class="shop-grid-nav">
+                                <ul class="nav nav-pills align-items-center" id="pills-tab" role="tablist">
+                                    <li> <span class="total-products text-capitalize">
+                                            @if (count($discountCoupons) > 0)
                                             {{ __('names.showing') }}
                                             @if ($discountCoupons->currentPage() !== $discountCoupons->lastPage())
-                                                {{ $discountCoupons->count() * $discountCoupons->currentPage() - $discountCoupons->count() + 1 . __('–') . $discountCoupons->count() * $discountCoupons->currentPage() }}
+                                            {{ $discountCoupons->count() * $discountCoupons->currentPage() - $discountCoupons->count() + 1 . __('–') . $discountCoupons->count() * $discountCoupons->currentPage() }}
                                             @else
-                                                @if ($discountCoupons->total() - $discountCoupons->count() === 0)
-                                                    {{ $discountCoupons->count() }}
-                                                @else
-                                                    {{ $discountCoupons->total() - $discountCoupons->count() . __('–') . $discountCoupons->total() }}
-                                                @endif
+                                            @if ($discountCoupons->total() - $discountCoupons->count() === 0)
+                                            {{ $discountCoupons->count() }}
+                                            @else
+                                            {{ $discountCoupons->total() - $discountCoupons->count() . __('–') . $discountCoupons->total() }}
+                                            @endif
                                             @endif
                                             {{ __('names.of') }}
                                             {{ $discountCoupons->total() . ' ' . __('names.entries') }}
-                                        @endif
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="col-lg-6 col-md-6">
-                                <div class="shop-shorting-right-content">
-                                </div>
-                            </div>
+                                            @endif
+                                    </li>
+                                </ul>
+                            </nav>
                         </div>
                     </div>
-                    <div class="row g-5">
-                        @forelse($discountCoupons as $discountCoupon)
-                            <div class="col-md-6">
-                                <div class="content-blog blog-grid d-flex justify-content-between align-items-center">
-                                    <div class="inner">
-                                        <h5 class="mb-1">{{ __('names.discountCouponCode') }}: {{ $discountCoupon->code }}
-                                        </h5>
-                                        <span class="filter-results">{{ __('names.discountCouponValue') }}:
-                                            €{{ number_format($discountCoupon->value, 2) }}</span>
-                                    </div>
-                                    @if ($discountCoupon->used)
-                                        <div>
-                                            <div class="border border-danger d-flex align-items-center justify-content-center px-4 py-3 text-danger fw-bold"
-                                                style="border-radius: 6px">
-                                                {{ __('names.used') }}
-                                            </div>
-                                        </div>
-                                    @else
-                                        <div>
-                                            <div class="border border-success d-flex align-items-center justify-content-center px-4 py-3 text-success fw-bold"
-                                                style="border-radius: 6px">
-                                                {{ __('names.active') }}
-                                            </div>
-                                        </div>
-                                    @endif
-                                </div>
-                            </div>
-                        @empty
-                            <span class="text-muted">{{ __('names.noDiscountCoupons') }}</span>
-                        @endforelse
-                    </div>
-                    @if (count($discountCoupons) > 10)
-                        <div class="text-center pt--20">
-                            {{ $discountCoupons->onEachSide(1)->links() }}
-                        </div>
-                    @endif
                 </div>
+                <div class="row g-5">
+                    @forelse($discountCoupons as $discountCoupon)
+                    <div class="col-md-6">
+                        <div class="brand-init style1 blog-grid d-flex justify-content-between align-items-center">
+                            <div class="inner">
+                                <h5 class="mb-1">{{ __('names.discountCouponCode') }}: {{ $discountCoupon->code }}
+                                </h5>
+                                <span class="filter-results">{{ __('names.discountCouponValue') }}:
+                                    €{{ number_format($discountCoupon->value, 2) }}</span>
+                            </div>
+                            @if ($discountCoupon->used)
+                            <div>
+                                <div class="border border-danger d-flex align-items-center justify-content-center px-4 py-3 text-danger fw-bold"
+                                    style="border-radius: 6px">
+                                    {{ __('names.used') }}
+                                </div>
+                            </div>
+                            @else
+                            <div>
+                                <div class="border border-success d-flex align-items-center justify-content-center px-4 py-3 text-success fw-bold"
+                                    style="border-radius: 6px">
+                                    {{ __('names.active') }}
+                                </div>
+                            </div>
+                            @endif
+                        </div>
+                    </div>
+                    @empty
+                    <span class="text-muted">{{ __('names.noDiscountCoupons') }}</span>
+                    @endforelse
+                </div>
+                @if (count($discountCoupons) > 10)
+                <div class="text-center pt--20">
+                    {{ $discountCoupons->onEachSide(1)->links() }}
+                </div>
+                @endif
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @push('css')
-    <style>
-        .filter-results {
-            margin-left: 0px;
-        }
-    </style>
+<style>
+    .filter-results {
+        margin-left: 0px;
+    }
+</style>
 @endpush
