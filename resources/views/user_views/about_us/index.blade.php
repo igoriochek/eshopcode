a433253604048aed3464af81734c686758b2bf5b
LastWeekNextDay <doesn@mail.ru>
2024-12-06 17:02:12 +0200

    Fix wrong localization for english locale in about us page

--- resources/views/user_views/about_us/index.blade.php
+++ resources/views/user_views/about_us/index.blade.php
@@ -14,7 +14,7 @@
                         <div class="section-title" style="text-align: justify;">
                             @if (app()->getLocale() == 'lt')
                             <p>UAB "Jodesta" – atestuota įmonė, įkurta 1997 metais, dirbanti energetikos sektoriuje. Įmonė atlieka įvairios paskirties išorės bei vidaus elektros inžinerinių tinklų projektavimo, montavimo bei remonto darbus. Taip pat vykdo elektros įrenginių techninę priežiūrą. Įmonėje dirba kompetentingi ir atestuoti vadovai bei aukštos kvalifikacijos specialistai, atliekantys projektavimo ir montavimo darbus.</p>
-                            @elseif (app()->getLocale() == 'ru')
+                            @elseif (app()->getLocale() == 'en')
                             <p>UAB "Jodesta" is a certified company established in 1997, working in the energy sector. The company carries out design, installation and repair works of external and internal electrical engineering networks for various purposes. It also carries out technical maintenance of electrical equipment. The company employs competent and certified managers and highly qualified specialists for design and installation work.</p>
                             @else
                             <p>ЗАО «Йодеста» - сертифицированная компания, основанная в 1997 году и работающая в энергетическом секторе. Компания выполняет проектные, монтажные и ремонтные работы внешних и внутренних электротехнических сетей различного назначения. Также осуществляется техническое обслуживание электрооборудования. В компании работают компетентные и аттестованные менеджеры и высококвалифицированные специалисты для выполнения проектных и монтажных работ.</p>
