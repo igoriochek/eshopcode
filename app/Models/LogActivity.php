8af7957b19a0de461a477cb8642490e18bfbc25d
edgarsem <90824295+edgarsem@users.noreply.github.com>
2025-02-10 14:27:28 +0200

    Updated LogActivity, language dropdown, admin customers table

--- app/Models/LogActivity.php
+++ app/Models/LogActivity.php
@@ -26,7 +26,8 @@ class LogActivity extends Model
     public function scopeSearch($query, $keywords)
     {
 //        return $query->where('activity', 'RLIKE', '[[:<:]]'.$keywords.'[[:>:]]');
-        return $query->where('activity', 'RLIKE', '\\'.$keywords.'\\b');
+        // return $query->where('activity', 'RLIKE', '\\'.$keywords.'\\b');
+        return $query->where('activity', 'RLIKE', '[[:<:]]' . addslashes($keywords) . '[[:>:]]');
     }
 
     /**
--- resources/views/layouts/dropdowns/language_dropdown.blade.php
+++ resources/views/layouts/dropdowns/language_dropdown.blade.php
@@ -1,7 +1,8 @@
 <div class="dropdown-menu dropdown-menu-arrow-centered min-width-0" aria-labelledby="dropdownCurrency" style="">
     @foreach (config('translatable.locales') as $locale)
-        <a class="dropdown-item" href="/lang/{{ $locale }}" style="font-size: .9rem;">
-            {{ strtoupper($locale) }}
-        </a>
+    <a class="dropdown-item" href="/lang/{{ $locale }}" style="font-size: .9rem;">
+        <img src="{{asset('/images/flag-' . $locale . '.png')}}" alt="{{$locale}}" style="max-width: 15px; display: inline-block; margin-right: 5px;"/>
+        {{ strtoupper($locale) }}
+    </a>
     @endforeach
 </div>
