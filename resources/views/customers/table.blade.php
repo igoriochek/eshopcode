0d6eca1b4dfa1e31040b0f14c67df6051ebb3f7c
edgarsem <90824295+edgarsem@users.noreply.github.com>
2025-02-11 13:52:02 +0200

    Minor updates

--- resources/views/customers/table.blade.php
+++ resources/views/customers/table.blade.php
@@ -2,17 +2,20 @@
     <table class="table" id="categories">
         <thead>
         <tr>
+            <th>{{__('table.userId')}}</th>
             <th>{{__('table.userName')}}</th>
             <th>{{__('table.email')}}</th>
+            <th>{{__('table.date')}}</th>
             <th>{{__('table.action')}}</th>
         </tr>
         </thead>
         <tbody>
         @foreach($customers as $customer)
             <tr>
-
+                <td>{{$customer->id}}</td>
                 <td>{{$customer->name}}</td>
                 <td>{{$customer->email}}</td>
+                <td>{{$customer->created_at}}</td>
 
                 <td width="120">
                     {!! Form::open(['route' => ['customers.destroy', $customer->id], 'method' => 'delete']) !!}
--- resources/views/layouts/dropdowns/language_dropdown.blade.php
+++ resources/views/layouts/dropdowns/language_dropdown.blade.php
@@ -1,6 +1,7 @@
 <div class="dropdown-menu dropdown-menu-arrow-centered min-width-0" aria-labelledby="dropdownCurrency" style="">
     @foreach (config('translatable.locales') as $locale)
         <a class="dropdown-item" href="/lang/{{ $locale }}" style="font-size: .8em">
+            <img src="{{asset('/images/flag-' . $locale . '.png')}}" alt="{{$locale}}" style="max-width: 15px; display: inline-block; margin-right: 5px;"/>
             {{ strtoupper($locale) }}
         </a>
     @endforeach
