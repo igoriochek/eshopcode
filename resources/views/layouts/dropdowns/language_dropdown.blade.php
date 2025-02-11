1c16f957499b26118f976636b354410edf4c8b27
edgarsem <90824295+edgarsem@users.noreply.github.com>
2025-02-11 14:25:01 +0200

    Minor updates and fixes

--- resources/views/layouts/dropdowns/language_dropdown.blade.php
+++ resources/views/layouts/dropdowns/language_dropdown.blade.php
@@ -2,8 +2,17 @@
     @foreach (config('translatable.locales') as $locale)
         <li class="language__items">
             <a class="language__text" href="/lang/{{ $locale }}">
+                <img src="{{asset('/images/flag-' . $locale . '.png')}}" alt="{{$locale}}" style="max-width: 15px; display: inline-block; margin-right: 5px;"/>
                 {{ strtoupper($locale) }}
             </a>
         </li>
     @endforeach
 </ul>
+
+<style>
+    .language__text {
+        display: flex;
+        align-items: center;
+    }
+
+</style>
