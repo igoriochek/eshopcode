d6b898232342a312dd1c855fd06bed6479e8bfa3
edgarsem <90824295+edgarsem@users.noreply.github.com>
2025-02-18 19:25:15 +0200

    Fixed order creation even if user session is lost and transaction response message for Paysera callback

--- routes/web.php
+++ routes/web.php
@@ -167,9 +167,6 @@ Route::group(array('prefix' => 'user', 'middleware' => ['auth', 'cookie-consent'
     Route::get('checkout', [OrderController::class, 'checkout'])->name('checkout');
     Route::post('checkout/preview', [OrderController::class, 'checkoutPreview'])->name('checkout-preview');
     Route::post('pay', [PayController::class, 'index'])->name('pay');
-    Route::get('pay/accept/{id}', [PayController::class, 'accept'])->where('id', '[0-9]+')->name('pay-accept');
-    Route::get('pay/cancel/{id}', [PayController::class, 'cancel'])->where('id', '[0-9]+')->name('pay-cancel');
-    Route::get('pay/callback/{id}', [PayController::class, 'callback'])->where('id', '[0-9]+')->name('pay-callback');
     //Route::get('viewOrders', [\App\Models\Order::class, 'viewAllOrders'])->name('viewallorders');
     Route::get('rootorders', [OrderController::class, 'indexOrders'])->name('rootorders');
     Route::get('rootoreturns', [ReturnsController::class, 'indexReturns'])->name('rootoreturns');
@@ -188,6 +185,11 @@ Route::group(array('prefix' => 'user', 'middleware' => ['auth', 'cookie-consent'
     Route::get('messenger/add', MessengerAdd::class)->name('livewire.messenger.add');
     Route::get('messenger/{id}', MessengerShow::class)->name('livewire.messenger.show');
 });
+
+// Payment
+Route::get('pay/accept/{userId}/{id}', [PayController::class, 'accept'])->where('userId', '[0-9]+')->where('id', '[0-9]+')->name('pay-accept');
+Route::get('pay/cancel/{userId}/{id}', [PayController::class, 'cancel'])->where('userId', '[0-9]+')->where('id', '[0-9]+')->name('pay-cancel');
+Route::get('pay/callback/{userId}/{id}', [PayController::class, 'callback'])->where('userId', '[0-9]+')->where('id', '[0-9]+')->name('pay-callback');
 
 //Route::get("homepage", [App\Http\Controllers\HomeController::class, 'userhomepage'])->name('userhomepage');
 Route::get("rootcategories", [CategoryController::class, 'userRootCategories'])->name('rootcategories');
